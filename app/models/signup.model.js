5069a444d6b2cb62a6f0daa157c7708697e89c49
Jorge Parra <jorge.luiz.alves.parra@outlook.com>
2020-12-14 18:59:32 -0300

    i created the model signup and registered it in the sequelize.

---
+++ app/models/signup.model.js
@@ -0,0 +1,42 @@
+module.exports = (sequelize, Sequelize) => {
+    const Signup = sequelize.define("signup", {
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: {
+                    args: true,
+                    msg: 'O valor passado não é um email.'
+                }
+            }
+        },
+        studentNumber: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                len: {
+                    args: 6,
+                    msg: 'O registro acadêmico deve conter 6 numeros.'
+                }
+            }
+        },
+        cpf: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                len: {
+                    args: 11,
+                    msg: 'O registro acadêmico deve conter 11 numeros.'
+                }
+            }
+        }
+    });
+  
+    return Signup;
+};
